05d4a7a745f08cab56f613453c4f13dacbf34579
Vlo Baxdasaryan <vlovlo1994@mail.ru>
2023-07-01 18:41:10 +0400

    Add project files.

---
+++ Operator/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point a = new Point(16, 14);
+            Point b = new Point(8, 7);
+
+            Point c = a + b;
+            c.Print();
+
+            Point d = a - b;
+            d.Print();
+
+            Point e = a / b;
+            e.Print();
+
+            Point f = a * b;
+            f.Print();
+
+            int[] arr = { 5, 6, 7, 8, 9 };
+            int[] arr1 = { 5, 6, 8, 90, 0 };
+
+
+            SumArray a1 = new SumArray(arr);
+            SumArray b1 = new SumArray(arr1);
+            SumArray c1 = a1 + b1;
+
+            c1.PrintArray(c1.Array);
+        }
+    }
+}
---
+++ Operator/SumArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator
+{
+    internal class SumArray
+    {
+        public int[] Array { get; set; }
+
+        public SumArray(int[] a)
+        {
+            Array = a;
+        }
+
+        public static SumArray operator +(SumArray a, SumArray b)
+        {
+            int[] result = new int[a.Array.Length];
+
+            for (int i = 0; i < a.Array.Length; i++)
+            {
+                result[i] = a.Array[i] + b.Array[i];
+            }
+            return new SumArray(result);
+
+        }
+
+        public void PrintArray(int[]arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]+"  ");
+            }
+        }
+
+    }
+}
